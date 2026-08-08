e702f50d871df85b506c501f9631d53cbb8901b1
Yahya Zulfikri <52540400+zulfikriyahya@users.noreply.github.com>
2026-08-08 21:11:31 +0700

    Update main.ino

--- firmware/esp32-c3-super-mini/main.ino
+++ firmware/esp32-c3-super-mini/main.ino
@@ -1,18 +1,22 @@
 #include <SPI.h>
 #include <MFRC522.h>
+
 #define SS_PIN 7
-#define RST_PIN 10
-#define BUZZER_PIN 3
+#define RST_PIN 3
+#define BUZZER_PIN 10
 #define SCK_PIN 4
 #define MISO_PIN 5
 #define MOSI_PIN 6
+
 MFRC522 rfid(SS_PIN, RST_PIN);
+
 void beepSuccess()
 {
     tone(BUZZER_PIN, 2000);
     delay(120);
     noTone(BUZZER_PIN);
 }
+
 void beepStartup()
 {
     tone(BUZZER_PIN, 1800);
@@ -23,6 +27,7 @@ void beepStartup()
     delay(80);
     noTone(BUZZER_PIN);
 }
+
 void setup()
 {
     Serial.begin(115200);
@@ -32,6 +37,7 @@ void setup()
     beepStartup();
     Serial.println("RFID Reader siap. Tempelkan kartu...");
 }
+
 void loop()
 {
     if (!rfid.PICC_IsNewCardPresent())
@@ -42,15 +48,22 @@ void loop()
     {
         return;
     }
-    uint32_t uidValue = 0;
-    for (byte i = 0; i < rfid.uid.size; i++)
+
+    uint32_t uidValue;
+    if (rfid.uid.size >= 4)
     {
-        uidValue = (uidValue << 8) | rfid.uid.uidByte[i];
+        uidValue = ((uint32_t)rfid.uid.uidByte[3] << 24) | ((uint32_t)rfid.uid.uidByte[2] << 16) | ((uint32_t)rfid.uid.uidByte[1] << 8) | rfid.uid.uidByte[0];
     }
+    else
+    {
+        uidValue = ((uint32_t)rfid.uid.uidByte[0] << 8) | rfid.uid.uidByte[1];
+    }
+
     char buf[11];
     sprintf(buf, "%010lu", (unsigned long)uidValue);
     Serial.println(buf);
     beepSuccess();
+
     rfid.PICC_HaltA();
     rfid.PCD_StopCrypto1();
     delay(150);
